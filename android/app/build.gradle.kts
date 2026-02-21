039f6bbbb98b6fdae47505b9ca1ac33b051918ed
jecem ben slama <benslemajecem@gmail.com>
2026-02-21 23:33:17 +0100

    fixed" flutter build apk "error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,3 +42,14 @@ android {
 flutter {
     source = "../.."
 }
+dependencies {
+
+    // ML Kit Text Recognition - Latin (required)
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+
+    // Optional language packs (required because plugin references them)
+    implementation("com.google.mlkit:text-recognition-chinese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-devanagari:16.0.0")
+    implementation("com.google.mlkit:text-recognition-japanese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-korean:16.0.0")
+}
